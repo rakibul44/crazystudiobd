d123fdfad711a8e6db8d6faeed15565bc3a1556f
MD Rakibul ahsan <mahsan201257@bscse.uiu.ac.bd>
2021-07-01 12:40:39 +0600

    slide area added

--- src/Js/custom.js
+++ src/Js/custom.js
@@ -31,7 +31,23 @@
         $('html').animate({ scrollTop : 0 }, 2000)
     })
     
-   
+    //owlcarousel
+    $('.slide-contant').owlCarousel({
+        loop:true,
+           autoplay:true,
+           autoplayTimeout:3000,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        } })
     
     
 }) (jQuery);
