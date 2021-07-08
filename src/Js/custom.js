cb37e9507a289fc18ede2fdc85982d6e96ac5ee6
MD Rakibul Ahsan <MD Rakibul Ahsan>
2021-07-09 01:19:57 +0600

    new js added

--- src/Js/custom.js
+++ src/Js/custom.js
@@ -75,5 +75,19 @@
     // // ripples
     // $('contact-overlay').ripples();
     
+    // top-arrow 
+    $(window).scroll( function(){
+        var dis = $(window).scrollTop();
+        if (dis > 350) {
+            $('.top-arrow').fadeIn();
+            }
+        else{
+            $('.top-arrow').fadeOut();
+        }
+    } );
+    
+    $('.top-arrow').on('click', function(){
+        $('html').animate({ scrollTop : 0 })
+    }) 
     
 }) (jQuery);
